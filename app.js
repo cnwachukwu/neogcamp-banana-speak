ecb16a9f43bcef8af3810d24b573d3a71b7ac77d
Paulson TM <paulsonchinedu-site>
2020-12-23 01:45:05 +0100

    button and event listener added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var prompt("what is your name? "),
+
+//var username = prompt("Enter your name")
+//var welcomeMessage = "this script works " + username
+//alert(welcomeMessage)
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked!")
+})
+
